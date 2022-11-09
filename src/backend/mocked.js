1e23364444f87879b04687858d10f08967eab50e
KlaytonJr <klaytonjunior@protonmail.com>
2022-11-09 13:17:49 -0300

    Change backend

--- src/backend/mocked.js
+++ src/backend/mocked.js
@@ -13,44 +13,57 @@ const publications = [
     {
         id: 2,
         title: "Mobile Apps",
-        content: `\tCom o avanço das tecnologias digitais, a palavra programação chega as rodas de conversa com um significado que já é antigo, mas que vem sendo discutido cada vez mais nos dias atuais. A palavra não se trata mais apenas do planejamento que você faz para as férias ou do rumo que dá ao dinheiro que sobra no final do mês, mas vai muito, muito além disso.\n
-            A programação que estamos falando aqui faz referência direta ao smartfone, tablet, notebook, ou qualquer outro aparelho eletrônico que você esteja utilizando neste exato momento para ler este texto. Isso porque, sem a programação com o seu sentido atual, você não conseguiria se comunicar facilmente com nenhuma dessas tecnologias.\n
-            De forma geral, a programação é um processo de escrita, testes e manutenção de programas de computadores. Esses programas, por sua vez, são compostos por conjuntos de instruções determinados pelo programador que descrevem tarefas a serem realizadas pela máquina e atendem diversas finalidades.\n 
-            O advento da programação está diretamente ligado à história dos computadores. A primeira programadora foi uma mulher chamada Ada Lovelace, que escreveu um código para possibilitar a utilização da máquina analítica de Charles Babbage, uma máquina robusta, de difícil comunicação, considerada a precursora dos computadores eletrônicos atuais.\n
-            Um século depois, os computadores ficaram cada vez mais fáceis de serem utilizados e a programação ganha cada vez mais espaço na criação dessas tecnologias. Mas, para compreender de verdade do que ela se trata, é importante conhecer dois elementos fundamentais que compõem todos os computadores modernos, que são o hardware e o software.`,
+        content: `\tUm aplicativo móvel, ou aplicativo móvel, é um aplicativo de software desenvolvido especificamente para ser executado em pequenos dispositivos sem fio, como smartphones, tablets e smartwatches, em vez de desktops ou laptops. Os aplicativos móveis são criados e atualizados com base nas limitações atuais e futuras de cada dispositivo móvel. No entanto, eles também podem ter funções especiais e exclusivas. Por exemplo, um aplicativo pode permitir que os usuários digitalizem um código QR com a câmera do celular.\n
+            Os aplicativos móveis foram inicialmente destinados a oferecer suporte à produtividade; recuperando contatos, agendando e-mails, gerenciando calendários. Seu amplo apelo incentivou os editores de aplicativos a criar aplicativos para uma ampla variedade de categorias, incluindo jogos, entretenimento, redes sociais e notícias. Felizmente, o desenvolvimento de aplicativos é facilitado com a ajuda do software de desenvolvimento móvel.\n
+            Android e iOS (Apple) são os sistemas operacionais móveis mais populares. Os usuários baixam aplicativos gratuitos e pagos por meio da Google Play Store e da Apple App Store.\n
+            Os aplicativos pagos devem ser pagos antes que os usuários possam baixá-los na loja de aplicativos. O lucro desses aplicativos é dividido entre os editores e a loja de aplicativos. Pelo contrário, aplicativos gratuitos podem ser baixados gratuitamente. O criador do aplicativo usa estratégias de monetização do aplicativo, como anúncios no aplicativo ou compras no aplicativo para gerar receita.`,
         url: "https://images.pexels.com/photos/50614/pexels-photo-50614.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         alt: "mão segurando um smartphone"
     },
     {
         id: 3,
         title: "Criando seu site",
-        content: `\tCom o avanço das tecnologias digitais, a palavra programação chega as rodas de conversa com um significado que já é antigo, mas que vem sendo discutido cada vez mais nos dias atuais. A palavra não se trata mais apenas do planejamento que você faz para as férias ou do rumo que dá ao dinheiro que sobra no final do mês, mas vai muito, muito além disso.\n
-            A programação que estamos falando aqui faz referência direta ao smartfone, tablet, notebook, ou qualquer outro aparelho eletrônico que você esteja utilizando neste exato momento para ler este texto. Isso porque, sem a programação com o seu sentido atual, você não conseguiria se comunicar facilmente com nenhuma dessas tecnologias.\n
-            De forma geral, a programação é um processo de escrita, testes e manutenção de programas de computadores. Esses programas, por sua vez, são compostos por conjuntos de instruções determinados pelo programador que descrevem tarefas a serem realizadas pela máquina e atendem diversas finalidades.\n 
-            O advento da programação está diretamente ligado à história dos computadores. A primeira programadora foi uma mulher chamada Ada Lovelace, que escreveu um código para possibilitar a utilização da máquina analítica de Charles Babbage, uma máquina robusta, de difícil comunicação, considerada a precursora dos computadores eletrônicos atuais.\n
-            Um século depois, os computadores ficaram cada vez mais fáceis de serem utilizados e a programação ganha cada vez mais espaço na criação dessas tecnologias. Mas, para compreender de verdade do que ela se trata, é importante conhecer dois elementos fundamentais que compõem todos os computadores modernos, que são o hardware e o software.`,
+        content: `\tO que é um site?
+            Um site é o conjunto de páginas ordenadas num servidor de internet cujo acesso ocorre a partir de um domínio. Assim como uma casa contém móveis como sofá, armário, mesa, cama, um site é constituído por páginas que têm textos, imagens, gráficos, vídeos e outras formas de mídia.\n
+        Para que serve um site?
+            Um site pode ter vários objetivos que variam desde visibilidade online até vendas. No caso de empresas, ele pode ser usado com essas duas finalidades: tanto para fazer o alcance na internet ser maior a ponto de deixar a marca mais forte e, assim, aumentar as chances de vender mais.\n
+        Como um site funciona?
+            Toda vez que uma pessoa visita um site, ela o acessa pela URL, ou seja o endereço do site que vem após o HTTP ou HTTPS (caso seja um site seguro, o que é recomendável). Em seguida, são exibidos os conteúdos do site hospedados num servidor e organizados em seu banco de dados.\n
+        Como é feito um site?
+            Um site é composto basicamente de: domínio para atribuir o endereço dele, servidor para armazenar todos os seus arquivos e CMS para gerir sua produção de conteúdos.`,
         url: "https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         alt: "código em um notebook"
     },
     {
         id: 4,
         title: "Melhores Temas",
-        content: `\tCom o avanço das tecnologias digitais, a palavra programação chega as rodas de conversa com um significado que já é antigo, mas que vem sendo discutido cada vez mais nos dias atuais. A palavra não se trata mais apenas do planejamento que você faz para as férias ou do rumo que dá ao dinheiro que sobra no final do mês, mas vai muito, muito além disso.\n
-            A programação que estamos falando aqui faz referência direta ao smartfone, tablet, notebook, ou qualquer outro aparelho eletrônico que você esteja utilizando neste exato momento para ler este texto. Isso porque, sem a programação com o seu sentido atual, você não conseguiria se comunicar facilmente com nenhuma dessas tecnologias.\n
-            De forma geral, a programação é um processo de escrita, testes e manutenção de programas de computadores. Esses programas, por sua vez, são compostos por conjuntos de instruções determinados pelo programador que descrevem tarefas a serem realizadas pela máquina e atendem diversas finalidades.\n 
-            O advento da programação está diretamente ligado à história dos computadores. A primeira programadora foi uma mulher chamada Ada Lovelace, que escreveu um código para possibilitar a utilização da máquina analítica de Charles Babbage, uma máquina robusta, de difícil comunicação, considerada a precursora dos computadores eletrônicos atuais.\n
-            Um século depois, os computadores ficaram cada vez mais fáceis de serem utilizados e a programação ganha cada vez mais espaço na criação dessas tecnologias. Mas, para compreender de verdade do que ela se trata, é importante conhecer dois elementos fundamentais que compõem todos os computadores modernos, que são o hardware e o software.`,
+        content: `\tPor Que Utilizar um Tema VS Code?\n
+            Os temas personalizados do VS Code permitem que você dê uma aparência totalmente única ao seu editor. Isso pode não parecer tão importante logo no início, mas trabalhar em um ambiente agradável pode aumentar significativamente a produtividade. Se você está codificando em mais de uma linguagem de programação, também pode usar um tema diferente para cada uma, então será mais fácil alternar seu cérebro entre as linguagens.\n
+            Basicamente existem dois tipos de temas: claro e escuro. Temas com fundo claro são normalmente recomendados para uso durante o dia, enquanto temas escuros são mais fáceis de visualizar à noite ou no escuro.\n
+            No geral, a maioria do desenvolvedores preferem o tema escuro para codificar.\n        
+            A seguir, confira os melhores temas para Visual Studio Code e os mais utilizados pelos desenvolvedores.\n
+            1- Night Owl
+            2- Drácula
+            3- One Dark Pro
+            4- Rainglow
+            5- Noctis
+            6- One Monokai Theme
+            7- Shades of Purple
+            8- Winter is Coming
+            9- Material Light
+            10- SynthWave ’84`,
         url: "https://images.pexels.com/photos/1181263/pexels-photo-1181263.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         alt: "dupla de mulheres analisando código"
     },
     {
         id: 5,
         title: "HTML para leigos",
-        content: `\tCom o avanço das tecnologias digitais, a palavra programação chega as rodas de conversa com um significado que já é antigo, mas que vem sendo discutido cada vez mais nos dias atuais. A palavra não se trata mais apenas do planejamento que você faz para as férias ou do rumo que dá ao dinheiro que sobra no final do mês, mas vai muito, muito além disso.\n
-            A programação que estamos falando aqui faz referência direta ao smartfone, tablet, notebook, ou qualquer outro aparelho eletrônico que você esteja utilizando neste exato momento para ler este texto. Isso porque, sem a programação com o seu sentido atual, você não conseguiria se comunicar facilmente com nenhuma dessas tecnologias.\n
-            De forma geral, a programação é um processo de escrita, testes e manutenção de programas de computadores. Esses programas, por sua vez, são compostos por conjuntos de instruções determinados pelo programador que descrevem tarefas a serem realizadas pela máquina e atendem diversas finalidades.\n 
-            O advento da programação está diretamente ligado à história dos computadores. A primeira programadora foi uma mulher chamada Ada Lovelace, que escreveu um código para possibilitar a utilização da máquina analítica de Charles Babbage, uma máquina robusta, de difícil comunicação, considerada a precursora dos computadores eletrônicos atuais.\n
-            Um século depois, os computadores ficaram cada vez mais fáceis de serem utilizados e a programação ganha cada vez mais espaço na criação dessas tecnologias. Mas, para compreender de verdade do que ela se trata, é importante conhecer dois elementos fundamentais que compõem todos os computadores modernos, que são o hardware e o software.`,
+        content: `\tHTML (Linguagem de Marcação de Hipertexto) é o código que você usa para estruturar uma página web e seu conteúdo. Por exemplo, o conteúdo pode ser estruturado em parágrafos, em uma lista com marcadores ou usando imagens e tabelas. Como o título sugere, este artigo fornecerá uma compreensão básica do HTML e suas funções.\n
+            Então, o que é HTML?\n
+            HTML não é uma linguagem de programação; é uma linguagem de marcação, usada para definir a estrutura do seu conteúdo. HTML consiste de uma série de elementos, que você usa para delimitar ou agrupar diferentes partes do conteúdo para que ele apareça ou atue de determinada maneira. As tags anexas podem transformar uma palavra ou imagem num hiperlink, pode colocar palavras em itálico, pode aumentar ou diminuir a fonte e assim por diante. Por exemplo, veja a seguinte linha de conteúdo:\n
+            Meu gatinho é muito mal humorado\n
+            Se quiséssemos que a linha permanecesse por si só, poderíamos especificar que é um parágrafo colocando-a em uma tag de parágrafo:\n
+            <p>Meu gatinho é muito mal humorado</p>`,
         url: "https://images.pexels.com/photos/577585/pexels-photo-577585.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         alt: "oculos próximo a um notebook focando o na tela"
     },
